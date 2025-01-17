780f9edd0b7cb48243300684aa5625fd4f4378f7
chiaralle <chiaraallegrino@gmail.com>
2025-01-17 17:06:39 +0100

    add: secondo esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+ 
 
 // Dichiara la funzione qui.
-
+function trovaIniziali(nomi){
+    let arrayIniziali=[];
+    for (let i = 0; i < nomi.length; i++) {
+        arrayIniziali.push(nomi[i].charAt(0));
+    } 
+    return arrayIniziali;    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(trovaIniziali(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
