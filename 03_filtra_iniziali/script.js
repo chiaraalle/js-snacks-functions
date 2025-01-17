003397d7ba100c17cbeedc7885424a866061d455
chiaralle <chiaraallegrino@gmail.com>
2025-01-17 17:41:14 +0100

    add: terzo esercizio

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function cercaNomi(nomi, lettera){
+    let arrayNomiSelezionati = [];
+    for (let i = 0; i < nomi.length; i++) {
+        if(nomi[i].charAt(0) == lettera) {
+            arrayNomiSelezionati.push(nomi[i]);
+        }
+    }
+    return arrayNomiSelezionati
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(cercaNomi(names, "A"))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
